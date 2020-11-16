3571195680fb1fbaeae49e8ee9c789afb6ffde0a
Savitsky Viacheslav <49763132+Swarfall@users.noreply.github.com>
2020-11-16 11:57:50 +0200

    First commit
    Add Xcode project

---
+++ LightITTestProgect/LightITTestProgect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightITTestProgect
+//
+//  Created by Вячеслав on 16.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
